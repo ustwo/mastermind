ae5b9f744e7d1023f3b652d1467253410bb947c8
Arnau Siches <arnau@ustwo.com>
2015-11-10 14:18:00 +0000

    Fix mitmproxy version specifier

--- proxyswitch/version.py
+++ proxyswitch/version.py
@@ -1,5 +1,5 @@
 from __future__ import (absolute_import, print_function, division)
 
-IVERSION = (0, 2, 0)
+IVERSION = (0, 2, 1)
 VERSION = ".".join(str(i) for i in IVERSION)
 MINORVERSION = ".".join(str(i) for i in IVERSION[:2])
