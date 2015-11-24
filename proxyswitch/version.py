b310b03231e9d91814d8d31f845d39b896d2d489
Arnau Siches <arnau@ustwo.com>
2015-11-24 15:13:05 +0000

    Bump version to 0.4.4

--- proxyswitch/version.py
+++ proxyswitch/version.py
@@ -1,5 +1,5 @@
 from __future__ import (absolute_import, print_function, division)
 
-IVERSION = (0, 4, 2)
+IVERSION = (0, 4, 4)
 VERSION = ".".join(str(i) for i in IVERSION)
 MINORVERSION = ".".join(str(i) for i in IVERSION[:2])
