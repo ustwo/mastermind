21ce08ea3071be7ddfa129c4e95ee14437b05327
Arnau Siches <arnau@ustwo.com>
2016-02-14 14:54:55 +0000

    Add label expansion, dot-prefixed

--- mastermind/uri.py
+++ mastermind/uri.py
@@ -1,11 +1,6 @@
-from collections import deque
 from urlparse import urlparse, urlsplit, parse_qs, parse_qsl
-try:
-   from urllib.parse import quote
-except ImportError:
-  from urllib import quote
 import uritemplate
-import re
+import rfc6570
 
 # If a URL has variables it is assumed to be a URI Template (RFC 6570)
 def is_template(url):
@@ -13,7 +8,7 @@ def is_template(url):
 
 def eq(a, b):
     # TODO: Implement templates
-    if is_template(a): foo(a, b)
+    # if is_template(a): foo(a, b)
 
     if is_template(a) or is_template(b): return False
 
@@ -24,6 +19,26 @@ def eq(a, b):
            match_schema(actual, expected) and \
            match_path(actual, expected) and \
            match_querystring(actual, expected)
+
+# def foo(a, b):
+#     actual = a
+#     expected = b
+
+#     if is_template(actual):
+#         ks = ["collection", "id", "q"]
+#         vs = ["people", "123", "1"]
+#         x = expand(actual, dict(zip(ks, vs)))
+#         expanded = urlsplit(x)
+
+#         print("x", x)
+#         print("p", expand_path(actual, vs))
+#         print("q", expand_query(actual, [("q", "1"), ("p", "2")]))
+#         print("pq", expand_path(expand_query(actual,
+#                                              [("q", "1"), ("p", "2")]),
+#                                 vs))
+
+#         print({"path": path_segments(expanded.path),
+#                "query": query_pairs(expanded.query)})
 
 
 # Receives a URI path (str) and returns its segments.
@@ -37,186 +52,6 @@ def query_pairs(query):
 def expand(uri, data):
     # return uritemplate.expand(uri, data)
     return x_expand(uri, data)
-
-# TODO: A simple #findall() gives back an _ordered_ list instead of an
-# _unordered_ set.  The benefit is the ability to create a dict from
-# the fragments picked from a url decomposition.
-def foo(a, b):
-    actual = a
-    expected = b
-
-    if is_template(actual):
-        ks = ["collection", "id", "q"]
-        vs = ["people", "123", "1"]
-        x = expand(actual, dict(zip(ks, vs)))
-        expanded = urlsplit(x)
-
-        print("x", x)
-        print("p", expand_path(actual, vs))
-        print("q", expand_query(actual, [("q", "1"), ("p", "2")]))
-        print("pq", expand_path(expand_query(actual,
-                                             [("q", "1"), ("p", "2")]),
-                                vs))
-
-        print({"path": path_segments(expanded.path),
-               "query": query_pairs(expanded.query)})
-
-
-RESERVED = ":/?#[]@!$&'()*+,;="
-OPERATOR = "+#./;?&|!@"
-MODIFIER = ":^"
-
-SEQ_TPL = re.compile("{([/;+#]?)([^/;+.?&#]+)}")
-# Sequence templates is a generalisation to apply in order a list of templates.
-# If an expression contains `{x,y}` it will expand x and y in order too.
-# Level 1 `{var}`
-# Level 1 TODO `{foo,bar}`
-# Level 2 `{+var}`
-# Level 2 TODO `{+foo,bar}`
-# Level 2 `{#var}`
-#
-# When partial is False and there are no seguments left it follows the RFC
-# so the result is empty.  But, when partial is True, the expression is kept
-# intact so you can apply multiple times the function with different
-# sequences:
-#
-#   expand_sequence("{var}", []) # => ""
-#   expand_sequence("{var}", [], partial=True) # => "{var}"
-#   expand_sequence("{foo}/{bar}", ["a"], partial=True) # => "a/{bar}"
-#
-def expand_sequence(tpl, segments, partial=False):
-    queue = deque(segments)
-    operators = "/#"
-    print("tpl", tpl)
-
-    def sub(m):
-        if len(queue) == 0 and partial: return m.group(0)
-        if len(queue) == 0: return ""
-
-        operator = m.group(1)
-        expression = m.group(2)
-        prefix = operator if (operator in operators) else ""
-        infix = operator if operator == "/" else ","
-        safe = RESERVED if operator else ""
-
-        variable_list = map(lambda _: quote(queue.popleft(), safe=safe),
-                            expression.split(","))
-
-        return "{}{}".format(prefix, infix.join(variable_list))
-
-    r = SEQ_TPL.sub(sub, tpl)
-    print(r)
-    return r
-
-
-QUERY_TPL = re.compile("{\?([^}]+)}")
-# Level 4 `{?q,p}`
-def expand_query(tpl, pairs):
-
-    def sub(m):
-        expression = m.group(1)
-        prefix = "?"
-        infix = "&"
-        keys = map(lambda x: x.strip(), expression.split(","))
-        tokens = ["{}={}".format(x, y) for x, y in pairs
-                                       if any(map(lambda k: x == k, keys))]
-
-        if len(tokens) == 0: return ""
-
-        return "{}{}".format(prefix, infix.join(tokens))
-
-    return QUERY_TPL.sub(sub, tpl)
-
-
-# Extracted from uritemplate-py
-def x_expand(template, variables):
-    TOSTRING = {
-        "" : uritemplate._tostring,
-        "+": uritemplate._tostring,
-        "#": uritemplate._tostring,
-        ";": uritemplate._tostring_semi,
-        "?": uritemplate._tostring_query,
-        "&": uritemplate._tostring_query,
-        "/": uritemplate._tostring_path,
-        ".": uritemplate._tostring_path,
-    }
-    RESERVED = ":/?#[]@!$&'()*+,;="
-    OPERATOR = "+#./;?&|!@"
-    MODIFIER = ":^"
-    TEMPLATE = re.compile("{([^\}]+)}")
-    """
-    Expand template as a URI Template using variables.
-    """
-    def _sub(match):
-        expression = match.group(1)
-        operator = ""
-        if expression[0] in OPERATOR:
-            operator = expression[0]
-            varlist = expression[1:]
-        else:
-            varlist = expression
-
-        safe = ""
-        if operator in ["+", "#"]:
-            safe = RESERVED
-        varspecs = varlist.split(",")
-        varnames = []
-        defaults = {}
-        for varspec in varspecs:
-            default = None
-            explode = False
-            prefix = None
-            if "=" in varspec:
-                varname, default = tuple(varspec.split("=", 1))
-            else:
-                varname = varspec
-            if varname[-1] == "*":
-                explode = True
-                varname = varname[:-1]
-            elif ":" in varname:
-                try:
-                    prefix = int(varname[varname.index(":")+1:])
-                except ValueError:
-                    raise ValueError("non-integer prefix '{0}'".format(
-                       varname[varname.index(":")+1:]))
-                varname = varname[:varname.index(":")]
-            if default:
-                defaults[varname] = default
-            varnames.append((varname, explode, prefix))
-
-        retval = []
-        joiner = operator
-        start = operator
-        if operator == "+":
-            start = ""
-            joiner = ","
-        if operator == "#":
-            joiner = ","
-        if operator == "?":
-            joiner = "&"
-        if operator == "&":
-            start = "&"
-        if operator == "":
-            joiner = ","
-        for varname, explode, prefix in varnames:
-            if varname in variables:
-                value = variables[varname]
-                if not value and value != "" and varname in defaults:
-                    value = defaults[varname]
-            elif varname in defaults:
-                value = defaults[varname]
-            else:
-                continue
-            expanded = TOSTRING[operator](
-              varname, value, explode, prefix, operator, safe=safe)
-            if expanded is not None:
-                retval.append(expanded)
-        if len(retval) > 0:
-            return start + joiner.join(retval)
-        else:
-            return ""
-
-    return TEMPLATE.sub(_sub, template)
 
 
 # TODO: mitmproxy 0.16 will fix the flow.response.url anomaly so the match_*
---
+++ test/test_rfc6570.py
@@ -0,0 +1,69 @@
+import mastermind.rfc6570 as rfc
+
+var = "value"
+hello = "Hello World!"
+empty = ""
+path = "/foo/bar"
+x = "1024"
+y = "768"
+
+# Simple string expansion                       (Sec 3.2.2)
+def test_simple_string_expansion():
+    assert rfc.expand_sequence("{var}", [var]) == "value"
+    assert rfc.expand_sequence("{hello}", [hello]) == "Hello%20World%21"
+
+# Reserved string expansion                     (Sec 3.2.3)
+def test_reserved_string_expansion():
+    assert rfc.expand_sequence("{+var}", [var]) == "value"
+    assert rfc.expand_sequence("{+hello}", [hello]) == "Hello%20World!"
+    assert rfc.expand_sequence("{+path}/here", [path]) == "/foo/bar/here"
+    assert rfc.expand_sequence("here?ref={+path}", [path]) == "here?ref=/foo/bar"
+
+# Fragment expansion, crosshatch-prefixed       (Sec 3.2.4)
+def test_fragment_expansion():
+    assert rfc.expand_sequence("X{#var}", [var]) == "X#value"
+    assert rfc.expand_sequence("X{#hello}", [hello]) == "X#Hello%20World!"
+
+# String expansion with multiple variables      (Sec 3.2.2)
+def test_string_expansion_with_multiple_variables():
+    assert rfc.expand_sequence("map?{x,y}", [x, y]) == "map?1024,768"
+    assert rfc.expand_sequence("{x,hello,y}", [x, hello, y]) == "1024,Hello%20World%21,768"
+
+# Reserved expansion with multiple variables    (Sec 3.2.3)
+def test_reserved_expansion_with_multiple_variables():
+    assert rfc.expand_sequence("{+x,hello,y}", [x, hello, y]) == "1024,Hello%20World!,768"
+    assert rfc.expand_sequence("{+path,x}/here", [path, x]) == "/foo/bar,1024/here"
+
+# Fragment expansion with multiple variables    (Sec 3.2.4)
+def test_fragment_expansion_with_multiple_variables():
+    assert rfc.expand_sequence("{#x,hello,y}", [x, hello, y]) == "#1024,Hello%20World!,768"
+    assert rfc.expand_sequence("{#path,x}/here", [path, x]) == "#/foo/bar,1024/here"
+
+# Label expansion, dot-prefixed                 (Sec 3.2.5)
+def test_label_expansion():
+    assert rfc.expand_sequence("X{.var}", [var]) == "X.value"
+    assert rfc.expand_sequence("X{.x,y}", [x, y]) == "X.1024,768"
+
+
+
+# TODO: Review
+def test_expand_sequence():
+    assert rfc.expand_sequence("{var}", []) == ""
+    assert rfc.expand_sequence("{var}", [], partial=True) == "{var}"
+
+    assert rfc.expand_sequence("http://example.com", []) == "http://example.com"
+    assert rfc.expand_sequence("http://example.com/{x}", ["a"]) == "http://example.com/a"
+    assert rfc.expand_sequence("http://example.com/{x}/{y}", ["a", "b"]) == "http://example.com/a/b"
+    assert rfc.expand_sequence("http://example.com/{x}?q=1", ["a", "b"]) == "http://example.com/a?q=1"
+    assert rfc.expand_sequence("http://example.com/foo?q={x}", ["1"]) == "http://example.com/foo?q=1"
+    assert rfc.expand_sequence("http://example.com{+path}/here", ["/foo/bar"]) == "http://example.com/foo/bar/here"
+    assert rfc.expand_sequence("http://example.com/here?ref={+path}", ["/foo/bar"]) == "http://example.com/here?ref=/foo/bar"
+
+
+
+def test_expand_query():
+    assert rfc.expand_query("http://example.com", []) == "http://example.com"
+    assert rfc.expand_query("http://example.com/{?xyz}", [("xyz", "a")]) == "http://example.com/?xyz=a"
+    assert rfc.expand_query("http://example.com{?q,p}", [("q", "1"), ("p", "2"), ("r", 3)]) == "http://example.com?q=1&p=2"
+    assert rfc.expand_query("http://example.com{?p,q}", [("q", "1"), ("p", "2")]) == "http://example.com?q=1&p=2"
+    assert rfc.expand_query("http://example.com{?q}", [("q", "1"), ("p", "2")]) == "http://example.com?q=1"
