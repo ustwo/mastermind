2685d9b54d708c072b86ba441c8e85cf27114939
Arnau Siches <arnau@ustwo.com>
2016-02-16 13:58:45 +0000

    Add edge case for rules.status_code()

--- test/test_rules.py
+++ test/test_rules.py
@@ -44,12 +44,9 @@ def test_delay():
 
 
 def test_status_code():
-    assert r.status_code({'url': 'http://foo',
-                          'response': {'code': 500}}) == 500
-
-def test_status_code_casted():
-    assert r.status_code({'url': 'http://foo',
-                          'response': {'code': '500'}}) == 500
+    assert r.status_code({'response': {'code': 500}}) == 500
+    assert r.status_code({'response': {}}) == None
+    assert r.status_code({'response': {'code': '500'}}) == 500
 
 def test_body_filename_exists():
     assert r.body_filename(rule) == 'ok200.json'
